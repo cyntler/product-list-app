6487795047fe75c66955d08e2716fc4757a922eb
Damian Cyntler <damian@cyntler.com>
2023-05-30 19:58:57 +0200

    feat: add test runner for frontend and backend

---
+++ backend/src/__tests__/example.test.tsx
@@ -0,0 +1,11 @@
+import { test, expect, describe } from 'vitest';
+
+describe('example', () => {
+  test('example test 1', () => {
+    expect(true).toBeTruthy();
+  });
+
+  test('example test 2', () => {
+    expect(false).toBeFalsy();
+  });
+});
