a329e37733e3fcd12073219cc70203f3e5b1c553
Damian Cyntler <damian@cyntler.com>
2023-06-02 08:48:43 +0200

    feat: add redux store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import './index.css';
+import { store } from './store';
 import { App } from './components/App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
