4dfc00f3fc83e01f200988235707fdddadd7c601
Damian Cyntler <damian@cyntler.com>
2023-06-05 00:03:39 +0200

    feat: add manipulate endpoints to product service

--- frontend/src/services/productService.ts
+++ frontend/src/services/productService.ts
@@ -2,16 +2,54 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { Product } from '../../../shared/models';
 
-console.log('import.meta.env', import.meta.env);
-
 export const productService = createApi({
   reducerPath: 'productService',
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_URL }),
+  tagTypes: ['Products'],
   endpoints: (builder) => ({
     getProducts: builder.query<Product[], void>({
       query: () => '/v1/products',
+      providesTags: ['Products'],
+    }),
+    getProduct: builder.query<Product, { id: Product['_id'] }>({
+      query: ({ id }) => `/v1/products/${id}`,
+      providesTags: (_result, _error, { id }) => [{ type: 'Products', id }],
+    }),
+    addProduct: builder.mutation<void, { data: Omit<Partial<Product>, '_id'> }>(
+      {
+        query: ({ data }) => ({
+          url: `/v1/products`,
+          method: 'POST',
+          body: data,
+        }),
+        invalidatesTags: ['Products'],
+      },
+    ),
+    updateProduct: builder.mutation<
+      void,
+      { id: Product['_id']; data: Omit<Partial<Product>, '_id'> }
+    >({
+      query: ({ id, data }) => ({
+        url: `/v1/products/${id}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Products'],
+    }),
+    deleteProduct: builder.mutation<void, { id: Product['_id'] }>({
+      query: ({ id }) => ({
+        url: `/v1/products/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Products'],
     }),
   }),
 });
 
-export const { useGetProductsQuery } = productService;
+export const {
+  useGetProductsQuery,
+  useGetProductQuery,
+  useAddProductMutation,
+  useUpdateProductMutation,
+  useDeleteProductMutation,
+} = productService;
