747c49c64a5fb10f0b41a6ffd0e31648e6bab211
Damian Cyntler <damian@cyntler.com>
2023-06-05 00:06:12 +0200

    fix: app component styles

--- frontend/src/components/App/App.style.ts
+++ frontend/src/components/App/App.style.ts
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 import { styleFor } from '../../utils/mediaQueries';
 
 export const Container = styled.main`
-  padding: 2rem;
+  padding: 1.5rem;
   max-width: 1200px;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
 
   ${styleFor('desktop')} {
     padding: 4rem;
