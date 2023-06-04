81fc1e1839311cbbd45243af24883cc7b3bac0b8
Damian Cyntler <damian@cyntler.com>
2023-06-05 00:02:13 +0200

    feat: add styled-components theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import 'rsuite/dist/rsuite-no-reset.min.css';
 
 import './index.css';
 import { store } from './store';
+import { theme } from './theme';
 import { App } from './components/App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 );
---
+++ frontend/src/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#DFDFDF',
+    red: '#E01F1F',
+    orange: '#EAB25E',
+  },
+};
+
+export type Theme = typeof theme;
