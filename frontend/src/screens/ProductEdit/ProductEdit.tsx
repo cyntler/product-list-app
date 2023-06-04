26fc6ba79c4523f498bb0a3e6e369e2d3c8d0319
Damian Cyntler <damian@cyntler.com>
2023-06-05 00:03:06 +0200

    feat: update product edit screen

--- frontend/src/screens/ProductEdit/ProductEdit.tsx
+++ frontend/src/screens/ProductEdit/ProductEdit.tsx
@@ -1,5 +1,35 @@
-export const ProductEdit = () => (
-  <>
-    <h1>Product Edit</h1>
-  </>
-);
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { ScreenHeading } from '../../components/ScreenHeading';
+import { ProductForm } from '../../components/ProductForm';
+import { useGetProductQuery } from '../../services/productService';
+import { Loader } from '../../components/Loader';
+import { Subheading } from '../../components/Subheading';
+import { useToast } from '../../hooks/useToast';
+
+export const ProductEdit = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { showToast } = useToast();
+
+  const { data: product, isLoading } = useGetProductQuery({ id: id || '' });
+
+  const handleSuccess = () => {
+    showToast('The product has been saved!', 'success');
+    navigate('/');
+  };
+
+  return (
+    <>
+      <ScreenHeading backBtn>Edit Product</ScreenHeading>
+      <Subheading>
+        Complete the form below to edit product „{product?.name}”.
+      </Subheading>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ProductForm editProduct={product} onSuccess={handleSuccess} />
+      )}
+    </>
+  );
+};
