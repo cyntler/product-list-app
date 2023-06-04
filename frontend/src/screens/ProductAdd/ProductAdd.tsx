c0919fb5da44e4caaf44c8ad6714d4a881d459a7
Damian Cyntler <damian@cyntler.com>
2023-06-05 00:02:53 +0200

    feat: update product add screen

--- frontend/src/screens/ProductAdd/ProductAdd.tsx
+++ frontend/src/screens/ProductAdd/ProductAdd.tsx
@@ -1,5 +1,24 @@
-export const ProductAdd = () => (
-  <>
-    <h1>Product Add</h1>
-  </>
-);
+import { useNavigate } from 'react-router-dom';
+
+import { ProductForm } from '../../components/ProductForm';
+import { ScreenHeading } from '../../components/ScreenHeading';
+import { Subheading } from '../../components/Subheading';
+import { useToast } from '../../hooks/useToast';
+
+export const ProductAdd = () => {
+  const navigate = useNavigate();
+  const { showToast } = useToast();
+
+  const handleSuccess = () => {
+    showToast('The product has been added!', 'success');
+    navigate('/');
+  };
+
+  return (
+    <>
+      <ScreenHeading backBtn>Add Product</ScreenHeading>
+      <Subheading>Complete the form below to add a new product.</Subheading>
+      <ProductForm onSuccess={handleSuccess} />
+    </>
+  );
+};
